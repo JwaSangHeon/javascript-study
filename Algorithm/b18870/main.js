343902cea412face3147b1f72d7b1afb03543aaf
JwaSangHeon <whk4051@naver.com>
2023-05-21 19:54:02 +0900

    정리

--- Algorithm/b18870/main.js
+++ Algorithm/b18870/main.js
@@ -1,28 +1,13 @@
-const fs = require('fs');
-const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-const input = fs.readFileSync(filePath).toString().trim().split('\n');
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
 function solution(input) {
-  const inputlength = input.shift();
-  let newArray = input[0].split(' ');
-  // console.log(newArray);
-  // console.log(input);
-  newArray = newArray.map(x => +x);
-  let result=[];
-  // console.log(newArray);
-  for(let i = 0; i < newArray.length; i++){
-    const compareNum = newArray[i];
-    let count = 0;
-    let removeOverlap = [];
-    for(let j = 0; j < newArray.length; j++){
-        if(compareNum > newArray[j] && !(removeOverlap.includes(newArray[j]))){
-          count++;
-        }
-        removeOverlap.push(newArray[j]);
-    }
-    result.push(count);
-  }
-  console.log(result.join(' '));
+  const space = input.shift();
+  console.log(space);
+  console.log(input);
+  let firstWhiteCase = 0;
+  let firstBlackCase = 0;
 }
 
 solution(input);
--- Algorithm/b23968/main.js
+++ Algorithm/b23968/main.js
@@ -1,28 +1,38 @@
-const fs = require('fs');
-const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-const input = fs.readFileSync(filePath).toString().trim().split('\n');
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
 function solution(input) {
-  const inputlength = input.shift();
-  let newArray = input[0].split(' ');
-  // console.log(newArray);
-  // console.log(input);
-  newArray = newArray.map(x => +x);
-  let result=[];
-  // console.log(newArray);
-  for(let i = 0; i < newArray.length; i++){
-    const compareNum = newArray[i];
-    let count = 0;
-    let removeOverlap = [];
-    for(let j = 0; j < newArray.length; j++){
-        if(compareNum > newArray[j] && !(removeOverlap.includes(newArray[j]))){
-          count++;
+  const [inputLength, changeCount] = input[0].split(" ").map((ele) => +ele);
+  let result = [];
+  let count = 0;
+
+  let array = input[1].split(" ").map((ele) => +ele);
+  const sortedArray = input[1]
+    .split(" ")
+    .map((ele) => +ele)
+    .sort((a, b) => a - b);
+
+  while (!(array.toString() === sortedArray.toString())) {
+    for (let i = 0; i < inputLength - 1; i++) {
+      if (array[i] > array[i + 1]) {
+        count++;
+
+        let tmp = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = tmp;
+        if (count === changeCount) {
+          result.push(array[i]);
+          result.push(array[i + 1]);
         }
-        removeOverlap.push(newArray[j]);
+      }
     }
-    result.push(count);
   }
-  console.log(result.join(' '));
+  if (count < changeCount) {
+    console.log(-1);
+  } else {
+    console.log(result.sort((a, b) => a - b).join(" "));
+  }
 }
 
 solution(input);
